31bee127a5b9b30cd4c06394f60db034112a5808
Eric Gallager <egall@gwmail.gwu.edu>
2014-10-23 16:29:25 -0400

    configure tweaks, silence some warnings
    Change-Id: I8075e112f872e6051d7cff812461870615310936

--- mhttp_main.c
+++ mhttp_main.c
@@ -64,66 +64,91 @@ int main(int argc, char *argv[])
     int len = 0;
     xht h;
 
-    if(argc < 4) { printf("usage: mhttp 'unique name' 12.34.56.78 80 '/optionalpath'\n"); return 1; }
+    if (argc < 4) {
+        printf("usage: mhttp 'unique name' 12.34.56.78 80 '/optionalpath'\n");
+        return 1;
+    }
 
     ip = inet_addr(argv[2]);
     local_ip = inet_addr("127.0.0.1");
     port = atoi(argv[3]);
     /* not sure if this will work, as the second argument is made up: */
     my_in_addr_struct = inet_makeaddr(ip, local_ip);
-    printf("Announcing .local site named '%s' to %s:%d and extra path '%s'\n",argv[1],inet_ntoa(my_in_addr_struct),port,argv[4]);
+    printf("Announcing .local site named '%s' to %s:%d and extra path '%s'\n",
+           argv[1], inet_ntoa(my_in_addr_struct), port, argv[4]);
 
-    signal(SIGINT,done);
-    signal(SIGHUP,done);
-    signal(SIGQUIT,done);
-    signal(SIGTERM,done);
+    signal(SIGINT, done);
+    signal(SIGHUP, done);
+    signal(SIGQUIT, done);
+    signal(SIGTERM, done);
     pipe(_zzz);
     _d = d = mdnsd_new(1,1000);
-    if((s = msock()) == 0) { printf("cannot create socket: %s\n",strerror(errno)); return 1; }
+    if ((s = msock()) == 0) {
+        printf("cannot create socket: %s\n", strerror(errno));
+        return 1;
+    }
 
-    sprintf(hlocal,"%s._http._tcp.local.",argv[1]);
-    sprintf(nlocal,"http-%s.local.",argv[1]);
-    r = mdnsd_shared(d,"_http._tcp.local.",QTYPE_PTR,120);
-    mdnsd_set_host(d,r,hlocal);
-    r = mdnsd_unique(d,hlocal,QTYPE_SRV,600,con,0);
-    mdnsd_set_srv(d,r,0,0,port,nlocal);
-    r = mdnsd_unique(d,nlocal,QTYPE_A,600,con,0);
-    mdnsd_set_raw(d,r,(unsigned char *)&ip,4);
-    r = mdnsd_unique(d,hlocal,16,600,con,0);
+    sprintf(hlocal, "%s._http._tcp.local.", argv[1]);
+    sprintf(nlocal, "http-%s.local.", argv[1]);
+    r = mdnsd_shared(d, "_http._tcp.local.", QTYPE_PTR, 120);
+    mdnsd_set_host(d, r, hlocal);
+    r = mdnsd_unique(d, hlocal, QTYPE_SRV, 600, con, 0);
+    mdnsd_set_srv(d, r, 0, 0, port, nlocal);
+    r = mdnsd_unique(d, nlocal, QTYPE_A, 600, con, 0);
+    mdnsd_set_raw(d, r, (unsigned char *)&ip, 4);
+    r = mdnsd_unique(d, hlocal, 16, 600, con, 0);
     h = xht_new(11);
-    if(argc == 5 && argv[4] && strlen(argv[4]) > 0) xht_set(h,"path",argv[4]);
+    if ((argc == 5) && argv[4] && (strlen(argv[4]) > 0)) {
+        xht_set(h, "path", argv[4]);
+    }
     packet = sd2txt(h, &len);
     xht_free(h);
-    mdnsd_set_raw(d,r,packet,len);
+    mdnsd_set_raw(d, r, packet, len);
     free(packet);
 
-    while(1) {
+    while (1) {
         tv = mdnsd_sleep(d);
         FD_ZERO(&fds);
-        FD_SET(_zzz[0],&fds);
-        FD_SET(s,&fds);
-        select(s+1,&fds,0,0,tv);
+        FD_SET(_zzz[0], &fds);
+        FD_SET(s, &fds);
+        select((s + 1), &fds, 0, 0, tv);
 
-        /* only used when we wake-up from a signal, shutting down */
-        if(FD_ISSET(_zzz[0],&fds)) read(_zzz[0],buf,MAX_PACKET_LEN);
-
-        if(FD_ISSET(s,&fds)) {
-            while((bsize = recvfrom(s,buf,MAX_PACKET_LEN,0,(struct sockaddr*)&from,&ssize)) > 0)
-            {
-                bzero(&m,sizeof(struct message));
-                message_parse(&m,buf);
-                mdnsd_in(d,&m,(unsigned long int)from.sin_addr.s_addr,from.sin_port);
-            }
-            if(bsize < 0 && errno != EAGAIN) { printf("cannot read from socket %d: %s\n",errno,strerror(errno)); return 1; }
+        /* only used when we wake-up from a signal, shutting down: */
+        if (FD_ISSET(_zzz[0], &fds)) {
+            read(_zzz[0], buf, MAX_PACKET_LEN);
         }
-        while(mdnsd_out(d,&m,&ip,&port)) {
+
+        if (FD_ISSET(s, &fds)) {
+            while ((bsize = recvfrom(s, buf, MAX_PACKET_LEN, 0,
+                                     (struct sockaddr*)&from,
+                                     &ssize)) > 0) {
+                bzero(&m, sizeof(struct message));
+                message_parse(&m, buf);
+                mdnsd_in(d, &m, (unsigned long int)from.sin_addr.s_addr,
+                         from.sin_port);
+            }
+            if ((bsize < 0) && (errno != EAGAIN)) {
+                printf("cannot read from socket %d: %s\n", errno,
+                       strerror(errno));
+                return 1;
+            }
+        }
+        /* in case 'ip' is of type 'in_addr_t', cast it: */
+        while (mdnsd_out(d, &m, (unsigned long int *)&ip, &port)) {
             bzero(&to, sizeof(to));
             to.sin_family = AF_INET;
             to.sin_port = port;
             to.sin_addr.s_addr = ip;
-            if(sendto(s,message_packet(&m),message_packet_len(&m),0,(struct sockaddr *)&to,sizeof(struct sockaddr_in)) != message_packet_len(&m))  { printf("cannot write to socket: %s\n",strerror(errno)); return 1; }
+            if (sendto(s, message_packet(&m), message_packet_len(&m), 0,
+                       (struct sockaddr *)&to,
+                       sizeof(struct sockaddr_in)) != message_packet_len(&m)) {
+                printf("cannot write to socket: %s\n", strerror(errno));
+                return 1;
+            }
         }
-        if(_shutdown) break;
+        if (_shutdown) {
+            break;
+        }
     }
 
     mdnsd_shutdown(d);
--- xht.c
+++ xht.c
@@ -34,11 +34,11 @@ int _xhter(const char *s)
 {
     /* ELF hash uses unsigned chars and unsigned arithmetic for portability */
     const unsigned char *name = (const unsigned char *)s;
-    unsigned long h = 0, g;
+    unsigned long h = 0UL, g;
 
-    while (*name)
-    { /* do some fancy bitwanking on the string */
-        h = (h << 4) + (unsigned long)(*name++);
+    while (*name) {
+        /* do some fancy bitwanking on the string: */
+        h = ((h << 4) + (unsigned long)(*name++));
         if ((g = (h & 0xF0000000UL))!=0) {
             h ^= (g >> 24);
         }
@@ -52,9 +52,13 @@ int _xhter(const char *s)
 
 xhn _xht_node_find(xhn n, const char *key)
 {
-    for(;n != 0; n = n->next) {
-        if(n->key != 0 && strcmp(key, n->key) == 0) {
+    int i;
+    for (i = 0; n != 0; n = n->next) {
+        if ((n->key != 0) && (strcmp(key, n->key) == 0)) {
             return n;
+        }
+        if (i >= 0) {
+            i++;
         }
     }
     return 0;
@@ -67,51 +71,53 @@ xht xht_new(int prime)
 
     xnew = (xht)malloc(sizeof(struct xht_struct));
     xnew->prime = prime;
-    xnew->zen = (xhn)malloc(sizeof(struct xhn_struct)*prime); /* array of xhn size of prime */
-    bzero(xnew->zen,sizeof(struct xhn_struct)*prime);
+    /* array of xhn size of prime: */
+    xnew->zen = (xhn)malloc(sizeof(struct xhn_struct) * prime);
+    bzero(xnew->zen, (sizeof(struct xhn_struct) * prime));
     return xnew;
 }
 
-/* does the set work, used by xht_set and xht_store */
+/* does the set work, used by the xht_set() and xht_store() functions: */
 xhn _xht_set(xht h, const char *key, void *val, char flag)
 {
     int i;
     xhn n;
 
-    /* get our index for this key */
-    i = _xhter(key) % h->prime;
+    /* get our index for this key: */
+    i = (_xhter(key) % h->prime);
 
-    /* check for existing key first, or find an empty one */
-    if((n = _xht_node_find(&h->zen[i], key)) == 0) {
-        for(n = &h->zen[i]; n != 0; n = n->next) {
-            if(n->val == 0) {
+    /* check for existing key first, or find an empty one: */
+    if ((n = _xht_node_find(&h->zen[i], key)) == 0) {
+        for (n = &h->zen[i]; n != 0; n = n->next) {
+            if (n->val == 0) {
                 break;
             }
         }
     }
 
-    /* if none, make a new one, link into this index */
-    if(n == 0) {
+    /* if none, make a new one, link into this index: */
+    if (n == 0) {
         n = (xhn)malloc(sizeof(struct xhn_struct));
         n->next = h->zen[i].next;
         h->zen[i].next = n;
     }
 
-    /* when flag is set, we manage their mem and free em first */
-    if(n->flag) {
-        free(n->key);
+    /* when flag is set, we manage their mem and free them first: */
+    if (n->flag) {
+        free((void *)n->key);
         free(n->val);
     }
 
     n->flag = flag;
     n->key = key;
     n->val = val;
-	/* TODO: this function should return something */
+
+    return n; /* hope that this is correct... */
 }
 
 void xht_set(xht h, const char *key, void *val)
 {
-    if(h == 0 || key == 0) {
+    if ((h == 0) || (key == 0)) {
         return;
     }
     _xht_set(h, key, val, 0);
@@ -121,15 +127,15 @@ void xht_store(xht h, const char *key, int klen, void *val, int vlen)
 {
     char *ckey, *cval;
 
-    if(h == 0 || key == 0 || klen == 0) {
+    if ((h == 0) || (key == 0) || (klen == 0)) {
         return;
     }
 
-    ckey = (char*)malloc(klen+1);
-    memcpy(ckey,key,klen);
+    ckey = (char*)malloc(klen + 1);
+    memcpy(ckey, key, klen);
     ckey[klen] = '\0';
-    cval = (void*)malloc(vlen+1);
-    memcpy(cval,val,vlen);
+    cval = (void*)malloc(vlen + 1);
+    memcpy(cval, val, vlen);
     cval[vlen] = '\0'; /* convenience, in case it was a string too */
     _xht_set(h, ckey, cval, 1);
 }
@@ -139,7 +145,8 @@ void *xht_get(xht h, const char *key)
 {
     xhn n;
 
-    if(h == 0 || key == 0 || (n = _xht_node_find(&h->zen[_xhter(key) % h->prime], key)) == 0) {
+    if ((h == 0) || (key == 0) ||
+        (n = _xht_node_find(&h->zen[_xhter(key) % h->prime], key)) == 0) {
         return 0;
     }
 
@@ -152,13 +159,15 @@ void xht_free(xht h)
     xhn n, f;
     int i;
 
-    if(h == 0) return;
+    if (h == 0) {
+        return;
+    }
 
-    for(i = 0; i < h->prime; i++) {
-        for(n = (&h->zen[i])->next; n != 0;) {
+    for (i = 0; i < h->prime; i++) {
+        for (n = (&h->zen[i])->next; n != 0;) {
             f = n->next;
-            if(n->flag) {
-                free(n->key);
+            if (n->flag) {
+                free((void *)n->key);
                 free(n->val);
             }
             free(n);
@@ -175,17 +184,19 @@ void xht_walk(xht h, xht_walker w, void *arg)
     int i;
     xhn n;
 
-    if(h == 0 || w == 0) {
+    if ((h == 0) || (w == 0)) {
         return;
     }
 
-    for(i = 0; i < h->prime; i++) {
-        for(n = &h->zen[i]; n != 0; n = n->next) {
-            if(n->key != 0 && n->val != 0) {
+    for (i = 0; i < h->prime; i++) {
+        for (n = &h->zen[i]; n != 0; n = n->next) {
+            if ((n->key != 0) && (n->val != 0)) {
                 (*w)(h, n->key, n->val, arg);
             }
         }
     }
+
+    return;
 }
 
 /* EOF */
